74dd7476db5cab89846b7435062a69b358f9fe11
zzayah <107320284+zzayah@users.noreply.github.com>
2024-06-26 15:24:46 -0600

    Package Structure Complete / Dev Envs Reorganized

---
+++ perovskite_assesment_package/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pvskite-assess",
+    version="0.0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'input_file_generation=pvskite.generation:create_input',
+            'out_file_processing=pvskite.processing:process_output'
+        ],
+    
+    },
+    install_requires=[
+        "ase",
+        "pandas",
+    ],
+    author=["Zayah Cortright", "Stephen Lim"],
+    author_email=["zayahcortright@gmail.com", "stephenlimzy2008@gmail.com"],
+    description="",
+    long_description=open("README.md").read(),
+    url="",
+)
--- pvskite-github-placeholder/tests/test_input_file_generation.py
+++
@@ -1,9 +0,0 @@
-from pvskiassess import Generation, Extraction
-
-# Example Usage:
-cluster_name = "your_cluster_name"
-cluster_command = "your_cluster_command"
-material = "SrCoO3_A.xyz"
-
-generator = Generation(cluster_name, cluster_command)
-generator.generate_files(material)
